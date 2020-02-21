2dd164f87c8e8b26eef751a2dff301cb6b1c6c8b
Curren_Wang <curren_wong@163.com>
2020-02-22 03:24:24 +0800

    Update notes and set fold elements
    Former-commit-id: 6db90088a40abce3de35cfe1fd671cd22cb47334

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -1,14 +1,29 @@
-$(document).ready(function () {
+// 渲染未被mkdocs捕捉到的公式，此处暂时用不上
+// $(document).ready(function () {
+//     renderMathInElement(document.body, {
+//         delimiters: [{
+//             left: "$$",
+//             right: "$$",
+//             display: true
+//         }, {
+//             left: "$",
+//             right: "$",
+//             display: false
+//         }]
+//     })
+// });
+
+// 渲染折叠公式
+$(document).ready($("math-details").each(function () {
     renderMathInElement(document.body, {
         delimiters: [{
             left: "$$",
             right: "$$",
             display: true
-        },  {
+        }, {
             left: "$",
             right: "$",
             display: false
         }]
     })
-});
-
+}));
