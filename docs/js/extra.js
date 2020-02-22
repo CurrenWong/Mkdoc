083758c25ff8e6227dd451f78d4fb873b5bca11f
Curren_Wang <curren_wong@163.com>
2020-02-23 02:45:37 +0800

    Update problems

--- docs/js/extra.js
+++ docs/js/extra.js
@@ -27,3 +27,20 @@ $(document).ready($("math-details").each(function () {
         }]
     })
 }));
+// 渲染标题5，使其即便不显示在目录上，也能够带有锚点和链接，
+$(document).ready(
+    $("h5").each(function () {
+        // 获取数值和文本
+        var str = $(this).text();
+        var num = str.replace(/[.]/ig, "").replace(/[^0-9]+/ig, '-');
+        if (num[0] == "-") {
+            num = num.substring(1, num.length)
+        }
+        // 清除内容
+        $(this).html("");
+        // 添加链接id属性
+        $(this).attr("id", num);
+        // 添加超链接
+        $(this).append('<a class="toclink" href="#' + num + '">' + str + '</a>');
+        $(this).append('<a class="headerlink" href="#' + num + '" title="Permanent link">#</a>');
+    }));
